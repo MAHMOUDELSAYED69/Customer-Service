bd9f26b78591e956ee606adc43d451bd33e77582
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-11-29 04:40:28 +0200

    Add customer service screen with Tawk.to integration and update UI styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_tawkto/flutter_tawk.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Color(0xffDC562B),
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -12,6 +20,33 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Customer Service App',
       debugShowCheckedModeBanner: false,
+      home: CustomerServiceScreen(),
+    );
+  }
+}
+
+class CustomerServiceScreen extends StatefulWidget {
+  const CustomerServiceScreen({super.key});
+
+  @override
+  State<CustomerServiceScreen> createState() => _CustomerServiceScreenState();
+}
+
+class _CustomerServiceScreenState extends State<CustomerServiceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Tawk(
+        directChatLink:
+            'https://tawk.to/chat/6749269d4304e3196aea2199/1idqrtm91',
+        visitor: TawkVisitor(
+          name: 'Mahmoud Elsayed',
+          email: 'mahmoudelsayed.dev@gmail.com',
+        ),
+        placeholder: const Center(
+          child: Text('Loading...'),
+        ),
+      ),
     );
   }
 }
